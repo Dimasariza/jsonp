11d829e62f9301f625ddb481beca4f639ceca33d
Dimas Ariza <roganda.sihombing11@gmail.com>
2025-04-19 01:29:30 +0700

    feat: add footer component (#4)

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import { Button } from '@components/ui/button'
 import JSONFormatter from '@pages/JSONFormatter'
 import Header from '@components/customs/header/header'
+import Footer from '@components/customs/footer/footer'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Footer />
     </>
   )
 }
