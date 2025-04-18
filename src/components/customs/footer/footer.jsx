a5d1cc27e9fd2a75c2cfb83dbfb957adf07bf3cc
Dimas Ariza <roganda.sihombing11@gmail.com>
2025-04-19 01:26:57 +0700

    chore: install react-icons package

--- src/components/customs/footer/footer.jsx
+++ src/components/customs/footer/footer.jsx
@@ -1,12 +1,14 @@
 import { Button } from '@/components/ui/button';
 import React from 'react';
+import { FaGithub } from "react-icons/fa";
+import { FaHandsHelping } from "react-icons/fa";
 
 const Footer = () => {
     return (
         <footer>
             <div className="flex items-center justify-between">
-                <Button variant="outline" color="red">View on Github</Button>
-                <Button variant="outline" color="red">Contribute</Button>
+                <Button variant="outline" color="red"><FaHandsHelping />View on Github</Button>
+                <Button variant="outline" color="red"><FaGithub />Contribute</Button>
             </div>
         </footer>
     );
